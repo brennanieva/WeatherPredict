89b6e38fcb7f25b804dc1bd64b1e4d59455b98b4
Brenna Nieva <brennanieva@Imperial-Star-Destroyer.local>
2019-11-12 11:19:45 -0800

    tommorrow's weather prediction vs the real temperature and margin of error

--- train.py
+++ train.py
@@ -13,6 +13,10 @@ ATTRS.append("Date")
 ATTRS.append("Precipitation")
 ATTRS.append("TMAX")
 ATTRS.append("TMIN")
+ATTRS.append("TOBS")
+#Time of Observation Bias
+#  different times of day exert different temperatures,  TOBS used to caluc
+
 
 ###############################################################################
 
@@ -22,6 +26,9 @@ def make_training_set(Jan2019):
     """ Compiles a training set from JAN2019 (CSV file) and creates dictionary (weather_records) with attributes labeled Station, Location, Date, Precipitation, Max Temperature (TMAX) and Minimum Temperature(TMIN)
     
     """
+
+
+
     weather_records = []
     # Read in file
     with open(Jan2019) as file:
@@ -67,9 +74,16 @@ def make_training_set(Jan2019):
 
     return weather_records
 
-#Ignore
-#euclidean_distance = math.sqrt((feature[0]-p[0])**2 +(feature[1]-p[1])**2
-#Ignore
+training_data = (make_training_set('TrainingSets/Jan2019.csv'))
+
+# def ClosestDate(training_data, today): 
+#     today = int(training_data[i].get('TMAX'))
+#     training_data = (make_training_set('TrainingSets/Jan2019.csv'))
+
+#     return training_data[min(range(len(training_data[i])), key = lambda i: abs(training_data[i]) - today)]  
+
+# print(ClosestDate(training_data,"2019-01-16")) 
+
 
 #Executes function and prints the results
 training_data = (make_training_set('TrainingSets/Jan2019.csv'))
@@ -94,35 +108,42 @@ for i in range(len(training_data)):
 
         print("Yesterday: ", yesterday)
         print("TMAX: ", training_data[i].get('TMAX'))
-        print("TMIN: ", training_data[i].get('TMIN'))
+#         print("TMIN: ", training_data[i].get('TMIN'))
         yesterday_tmax = int(training_data[i].get('TMAX'))
         yesterday_tmin = int(training_data[i].get('TMIN'))
-        print ("Yesterday's Temp Difference: ", YesterdayTempDifference)
+        # yesterday_prcp = int(training_data[i].get('PRCP'))
+#         print ("Yesterday's Temp Difference: ", YesterdayTempDifference)
+#         print()
 
 
-    #Today's data
+    # Today's data
     if training_data[i].get('Date')[8:] == today[8:]:
         TodayTempDifference = (training_data[i].get('TMAX')) - (training_data[i].get('TMIN'))
 
         print("Today: ", today)
         print("TMAX: ", training_data[i].get('TMAX'))
-        print("TMIN: ", training_data[i].get('TMIN'))
+    #     print("TMIN: ", training_data[i].get('TMIN'))
         
         today_tmax = int(training_data[i].get('TMAX'))
         today_tmin = int(training_data[i].get('TMIN'))
-        print ("Today's Temp Difference: ", TodayTempDifference)
+        # today_prcp = int(training_data[i].get('PRCP'))
+    #     print ("Today's Temp Difference: ", TodayTempDifference)
 
 
-
-
-        TomorrowMax = today_tmax + (today_tmax - yesterday_tmax )
-        print("Tomorrows Max Temp?: ", TomorrowMax)
-
-
-
+        #Sample TM calculations
+        print()
         
+        # print("Tomorrow Rainfall?: ", Tomorrow_prcp)
 
+    #Tomorrow Data
+    if training_data[i].get('Date')[8:] == tomorrow[8:]:
+        TomorrowTempPredict = today_tmax + (today_tmax - yesterday_tmax )
+        TomorrowTempReal = int(training_data[i].get('TMAX'))
+        error = len(range(TomorrowTempPredict,TomorrowTempReal))
+            # Tomorrow_prcp = today_prcp + (today_prcp - yesterday_prcp)
 
-
-
+            
+        print("Tomorrows Temp maybe who knows: ", TomorrowTempPredict)
+        print("Tommorrow Temp for real: ", TomorrowTempReal)
+        print("Margin of error: ", error)
 
